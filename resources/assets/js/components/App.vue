2c6804762c8447d2d713f46d25788ba07bd73eef
Juliane Blier <juliane@julianes-macbook-pro.home>
2017-08-31 20:59:56 +0200

    Added Vue component

---
+++ resources/assets/js/components/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="ze-container">
+    Hello World
+  </div>
+</template>
+
+<script>
+  export default {
+    mounted() {
+      console.log('Component mounted.')
+    }
+  }
+</script>
+
+<style lang="stylus">
+</style>
